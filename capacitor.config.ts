3ef9de4dd0ca778acae0ef20d3e0dbe3e9fa3ec0
DavidTorres <dadyotorres1403@gmail.com>
2022-04-23 02:12:44 -0500

    first commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.contact.plugin',
+  appName: 'contact',
+  webDir: 'dist/contact',
+  bundledWebRuntime: false
+};
+
+export default config;
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
+import { Plugins } from '@capacitor/core';
+import 'contacts-plugin';
+
+const { ContactsPlugin } = Plugins;
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
   title = 'contact';
+  contacts: any = [];
+  name: any;
+
+  async loadContact() {
+    this.contacts = (await ContactsPlugin['getContacts']('wherever')).results;
+    console.log("contacts", this.contacts)
+  }
 }
